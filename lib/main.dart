006edbdf3c08c4002bab0d343c88dcbf9d89657e
Soham Chakraborty <schakraborty8455@gmail.com>
2022-11-09 23:06:08 +0530

    Firebase Setup Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:examination_portal/LoginPortal.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+
+  );
+  runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MyApp()
-    )
-  );
+  ));
 }
 
 class MyApp extends StatelessWidget {
